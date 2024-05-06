fe4447ca99dd1c85a8222459fdf1e8e761095465
museop <museop92@gmail.com>
2024-05-06 22:10:17 +0900

    Follow the book contents

--- mix.exs
+++ mix.exs
@@ -4,9 +4,12 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      name: "Issues",
+      source_url: "https://github.com/pragdave/issues",
       deps: deps()
     ]
   end
@@ -21,8 +24,18 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 5.0"},
+      {:ex_doc, "~> 0.25"},
+      {:earmark, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
