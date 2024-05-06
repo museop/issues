e7788e714403d4c3bdb372b2ffd07881a40a3fe1
museop <museop92@gmail.com>
2024-05-06 16:15:38 +0900

    Initial commit of new project

---
+++ lib/issues.ex
@@ -0,0 +1,17 @@
+defmodule Issues do
+  @moduledoc """
+  Documentation for `Issues`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Issues.hello()
+      :world
+
+  """
+  def hello do
+    :world end
+end
