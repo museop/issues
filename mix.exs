80d74c460590640875dc9ddc1d7b8f8900427a30
museop <museop92@gmail.com>
2024-05-12 16:34:08 +0900

    test: use excoveralls for test coverage

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       start_permanent: Mix.env() == :prod,
       name: "Issues",
       source_url: "https://github.com/museop/issues",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -27,7 +34,8 @@ defmodule Issues.MixProject do
       {:httpoison, "~> 1.0"},
       {:poison, "~> 5.0"},
       {:ex_doc, "~> 0.25"},
-      {:earmark, "~> 1.4"}
+      {:earmark, "~> 1.4"},
+      {:excoveralls, "~> 0.14", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
